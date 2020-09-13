97dcb94821fe2c201501f751167c4139275cc339
liviacteixeira <liviacteixeira@gmail.com>
2020-09-13 16:01:36 +0100

    Fixed time bug

--- src/script.js
+++ src/script.js
@@ -1,7 +1,13 @@
 //Date
 function formatDate(date) {
   let hours = currentTime.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
   let minutes = currentTime.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
   let dayIndex = currentTime.getDay();
   let days = [
     "Sunday",
